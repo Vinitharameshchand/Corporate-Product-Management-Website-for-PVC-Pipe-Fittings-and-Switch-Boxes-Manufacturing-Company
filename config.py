aeca28ebf00feb648056e098460922c96f1df8da
vinitha <vinitharameshchand@gmail.com>
2026-02-17 23:43:46 +0530

    added all features

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'manufacturing-company-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root:@localhost/manufacturing_db'
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'manufacturing.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/images/products')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload size
